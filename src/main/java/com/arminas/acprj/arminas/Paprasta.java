562e1a45cd97ada3e4d8266eb12161ea9f0af2d1
arminas123 <35334646+arminas123@users.noreply.github.com>
2018-10-05 12:57:09 +0300

    meseges

--- src/main/java/com/arminas/acprj/arminas/Paprasta.java
+++ src/main/java/com/arminas/acprj/arminas/Paprasta.java
@@ -1,54 +1,106 @@
 package com.arminas.acprj.arminas;
 
 public class Paprasta {
-    String vardas;
-    String statusas;
 
-    public Paprasta(String vardas, String statusas) {
+    //    String vardas;
+//    String statusas;
+//
+//    public Paprasta(String vardas, String statusas) {
+//        this.vardas = vardas;
+//        this.statusas = statusas;
+//    }
+//
+//    public String getVardas() {
+//        return vardas;
+//    }
+//
+//    public String getStatusas() {
+//        return statusas;
+//    }
+//
+//    public String statusas() {
+//        return statusas();
+//    }
+//
+//    public void setStatusas(String statusas) {
+//        this.statusas = statusas;
+//    }
+//
+//    public void setVardas(String vardas) {
+//        this.vardas = vardas;
+//    }
+//
+//    public String vardas() {
+//        return vardas;
+//    }
+//
+//
+//    public void open() {
+//
+//        statusas = "open";
+//    }
+//
+//
+//    public void close() {
+//
+//        statusas = "closed";
+//
+//    }
+//
+//    public String israsymas() {
+//        return "Dooru vardas: " +  vardas + " Statusas duru: " + statusas;
+//
+//    }
+
+    public String statusas;
+
+
+    private String vardas;
+    private boolean atidaryta;
+
+
+    public Paprasta(String vardas, boolean atidaryta) {
         this.vardas = vardas;
-        this.statusas = statusas;
+        this.atidaryta = atidaryta;
     }
 
-    public String getVardas() {
-        return vardas;
+
+    public Paprasta(String vardas, String open) {
+
     }
 
-    public String getStatusas() {
-        return statusas;
-    }
-
-    public String statusas() {
-        return statusas();
-    }
-
-    public void setStatusas(String statusas) {
-        this.statusas = statusas;
-    }
 
     public void setVardas(String vardas) {
         this.vardas = vardas;
     }
 
-    public String vardas() {
-        return vardas;
+    public void setAtidaryta(boolean atidaryta) {
+        this.atidaryta = atidaryta;
     }
-
 
     public void open() {
 
-        statusas = "open";
+        atidaryta = true;
     }
-
 
     public void close() {
 
-        statusas = "closed";
+        atidaryta = false;
 
     }
 
-    public String israsymas() {
-        return "Dooru vardas: " + vardas + "Statusas duru: " + statusas;
 
+    public String israsymas() {
+        return "Dooru vardas: " + vardas + " Statusas duru: " + statusas;
+
+    }
+
+
+    public String statusas() {
+        if (atidaryta)
+            return "open";
+        else
+            return "closed";
     }
 }
 
